ffa5c21ebb313ca7187ee1a955d8995c79e24afc
Siddharth-73 <sharmasiddharth7373@gmail.com>
2025-12-09 03:48:20 +0530

    nitial project setup: Vite frontend, Express backend, MongoDB connection configured

---
+++ backend/src/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/api/health", (req, res) => {
+  res.json({ status: "OK", message: "Backend is running" });
+});
+
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
